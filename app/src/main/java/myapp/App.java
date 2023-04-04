8bd5067ee209ae119445e03f1e4a0894a44d0e98
Benjamin <Benjamin.Masters@Student.HTW-Berlin.de>
2023-04-04 20:40:23 +0200

    some ös

--- app/src/main/java/myapp/App.java
+++ app/src/main/java/myapp/App.java
@@ -5,7 +5,7 @@ package myapp;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hellö Wörld!";
     }
 
     public static void main(String[] args) {
